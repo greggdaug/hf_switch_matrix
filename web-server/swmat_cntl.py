fddb4d02a1c62b8f1e20c6dd18b30d7512edea48
Gregg Daugherty <gregg-daugherty@comcast.net>
2022-04-10 05:10:06 -0400

    control updates

--- web-server/swmat_cntl.py
+++ web-server/swmat_cntl.py
@@ -13,12 +13,13 @@ GPIO.setmode(GPIO.BCM)
 # GPIO 24 = GPIO_GEN5
 
 pins = {
-   17 : {'name' : 'relay1', 'state' : GPIO.LOW},
-   18 : {'name' : 'relay2', 'state' : GPIO.LOW},
-   27 : {'name' : 'relay3', 'state' : GPIO.LOW},
-   22 : {'name' : 'relay4', 'state' : GPIO.LOW},
-   23 : {'name' : 'relay5', 'state' : GPIO.LOW},
-   24 : {'name' : 'relay6', 'state' : GPIO.LOW}
+   17 : {'name' : 'relay1', 'state' : GPIO.LOW, 'antstate' : 'allterm'},
+   18 : {'name' : 'relay2', 'state' : GPIO.LOW, 'antstate' : 'allterm'},
+   27 : {'name' : 'relay3', 'state' : GPIO.LOW, 'antstate' : 'allterm'},
+   22 : {'name' : 'relay4', 'state' : GPIO.LOW, 'antstate' : 'allterm'},
+   23 : {'name' : 'relay5', 'state' : GPIO.LOW, 'antstate' : 'allterm'},
+   24 : {'name' : 'relay6', 'state' : GPIO.LOW, 'antstate' : 'allterm'},
+   25 : {'name' : 'dummy', 'state' : GPIO.LOW, 'antstate' : 'allterm'}
    }
 
 # Set each pin as an output and make it low:
@@ -58,13 +59,115 @@ def action(changePin, action):
    # For each pin, read the pin state and store it in the pins dictionary:
    for pin in pins:
       pins[pin]['state'] = GPIO.input(pin)
-
+      
    # Along with the pin dictionary, put the message into the template data dictionary:
    templateData = {
       'pins' : pins
    }
+      
+   # print(pins)
+      
+   if pins[17]['state'] == 0 and \
+      pins[18]['state'] == 0 and \
+      pins[27]['state'] == 0 and \
+      pins[22]['state'] == 0 and \
+      pins[23]['state'] == 0 and \
+      pins[24]['state'] == 0:
+          pins[25]['antstate'] = 'allterm'
+   elif pins[17]['state'] == 1 and \
+        pins[18]['state'] == 0 and \
+        pins[27]['state'] == 0 and \
+        pins[22]['state'] == 0 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant1rig1'
+   elif pins[17]['state'] == 1 and \
+        pins[18]['state'] == 0 and \
+        pins[27]['state'] == 0 and \
+        pins[22]['state'] == 1 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant1rig2'        
+   elif pins[17]['state'] == 0 and \
+        pins[18]['state'] == 1 and \
+        pins[27]['state'] == 0 and \
+        pins[22]['state']== 0 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant2rig1' 
+   elif pins[17]['state'] == 0 and \
+        pins[18]['state'] == 1 and \
+        pins[27]['state'] == 0 and \
+        pins[22]['state'] == 0 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant2rig2' 
+   elif pins[17]['state'] == 0 and \
+        pins[18]['state'] == 0 and \
+        pins[27]['state'] == 1 and \
+        pins[22]['state'] == 0 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant3rig1'
+   elif pins[17]['state'] == 0 and \
+        pins[18]['state'] == 0 and \
+        pins[27]['state'] == 1 and \
+        pins[22]['state'] == 0 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 1:
+            pins[25]['antstate'] = 'ant3rig2'
+   elif pins[17]['state'] == 1 and \
+        pins[18]['state'] == 1 and \
+        pins[27]['state'] == 0 and \
+        pins[22]['state'] == 0 and \
+        pins[23]['state'] == 1 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant1rig1_ant2rig2'
+   elif pins[17]['state'] == 1 and \
+        pins[18]['state'] == 1 and \
+        pins[27]['state'] == 0 and \
+        pins[22]['state'] == 1 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant1rig2_ant2rig1'
+   elif pins[17]['state'] == 1 and \
+        pins[18]['state'] == 1 and \
+        pins[27]['state'] == 0 and \
+        pins[22]['state'] == 1 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant1rig1_ant3rig2'
+   elif pins[17]['state'] == 1 and \
+        pins[18]['state'] == 0 and \
+        pins[27]['state'] == 1 and \
+        pins[22]['state'] == 1 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant1rig2_ant3rig1'
+   elif pins[17]['state'] == 0 and \
+        pins[18]['state'] == 1 and \
+        pins[27]['state'] == 1 and \
+        pins[22]['state'] == 0 and \
+        pins[23]['state'] == 0 and \
+        pins[24]['state'] == 1:
+            pins[25]['antstate'] = 'ant2rig1_ant3rig2'
+   elif pins[17]['state'] == 0 and \
+        pins[18]['state'] == 1 and \
+        pins[27]['state'] == 1 and \
+        pins[22]['state'] == 0 and \
+        pins[23]['state'] == 1 and \
+        pins[24]['state'] == 0:
+            pins[25]['antstate'] = 'ant2rig2_ant3rig1'
+   else:
+        pins[25]['antstate'] = 'undefined'
+            
+   # antstate = 'allterm'
+   # Along with the pin dictionary, put the message into the template data dictionary:
+   # templateData = {
+   #   antstate
+   # }
 
    return render_template('main.html', **templateData)
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+   app.run(host='0.0.0.0', port=80, debug=GPIO.HIGH)
